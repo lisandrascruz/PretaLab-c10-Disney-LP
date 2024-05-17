ab348771d91e516513333e5dbe5e97831613a038
Lisandra Cruz <lisandra.cruz@thoughtworks.com>
2024-05-17 19:40:12 -0300

    aula 2 ultimo exercicio

--- aula02/exercicio03.js
+++ aula02/exercicio03.js
@@ -1,3 +1,17 @@
 /*
   Crie um programa para medir o comprimento da circunferência.
 */
+// const pi = 3.14159265359;
+
+const raio = 4;
+
+// const area = pi * raio * raio;
+const area = Math.PI * Math.pow(raio, 2);
+console.log(
+  "A área do circulo de raio " + raio + "cm é igual a " + area.toFixed(2)
+);
+
+const comprimento = 2 * Math.PI * raio;
+console.log(
+  "A circunferencia do circulo de raio " + raio + "cm é igual a " + comprimento.toFixed(2)
+);
